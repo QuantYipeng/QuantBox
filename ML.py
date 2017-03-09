e7afc489b8e0d16971fad198805baf9219a52e0c
yipengli <yipeng_li@qq.com>
2017-03-10 00:16:10 +0800

    now can add correlated assets as much as you can

--- ML.py
+++ ML.py
@@ -9,91 +9,119 @@ import pickle
 
 
 def get_data(code=['300403', '000001'], days=200, l=1, data_file=''):
+    # parameters:
+    # l = how many history days used in prediction
+
+    def is_index(_code='000001'):
+        indices = ['000001', '399001', '399006']
+        for index in indices:
+            if index == _code:
+                return True
+        return False
+
     # get data
     if len(data_file):
         fn = data_file
         with open(fn, 'r') as f:
             content = pickle.load(f)  # read file and build object
-
-            temp = content.keys()
-            for t in temp:
-                if int(t) > 399000 and not int(t) > 399210:
-                    print t
-
-            hist = content[code[0]]
-            hist_index = content[code[1]]
+            hist = content.values()
     else:
         today = datetime.datetime.now().strftime('%Y-%m-%d')
         one_year_before = (datetime.datetime.now() - datetime.timedelta(days=days)).strftime('%Y-%m-%d')
-        hist = ts.get_h_data(code[0], start=one_year_before, end=today)  # reverse order (from now to past)
-        if code[1] == '000001' or code[1] == '399001':
-            hist_index = ts.get_h_data(code[1], index=True, start=one_year_before, end=today)  # reverse order (from now to past)
-        else:
-            hist_index = ts.get_h_data(code[1], start=one_year_before, end=today)
+        hist = []
+        for c in code:
+            if is_index(c):
+                hist.append(ts.get_h_data(c, index=True, start=one_year_before, end=today))  # reverse order
+            else:
+                hist.append(ts.get_h_data(c, start=one_year_before, end=today))  # reverse order (from now to past)
 
-    # reverse data order
-    hist = hist.sort_index(0)
-    hist_index = hist_index.sort_index(0)
+    # reverse data order to (from past to now)
+    for i in range(len(hist)):
+        hist[i] = hist[i].sort_index(0)
 
-    # drop the hist_index where there hist is no available
-    to_be_drop = []
-    if len(hist_index.index)-len(hist.index) != 0:
-        for i in range(len(hist_index.index)):
-            exist = 0
-            for j in range(len(hist.index)):
-                if hist_index.index[i] != hist.index[j]:
-                    exist = 0
-                else:
-                    exist = 1
-                    break
-            if exist == 0:
-                to_be_drop.append(hist_index.index[i])
-    hist_index = hist_index.drop(to_be_drop, axis=0)
+    # drop the unmatched data point
+    def match(hist_a, hist_b):
+        # return:
+        # hist_a, hist_b, is_too_many_a_dropped (If True -> drop hist_b)
+
+        to_be_drop_a = []
+        if len(hist_a.index) - len(hist_b.index) != 0:
+            for _i in range(len(hist_a.index)):
+                exist = 0
+                for _j in range(len(hist_b.index)):
+                    if hist_a.index[_i] != hist_b.index[_j]:
+                        exist = 0
+                    else:
+                        exist = 1
+                        break
+                if exist == 0:
+                    to_be_drop_a.append(hist_a.index[_i])
+        nb_drop_a = len(to_be_drop_a)
+        if nb_drop_a > 10:
+            return hist_a, hist_b, True
+        hist_a = hist_a.drop(to_be_drop_a, axis=0)
+
+        to_be_drop_b = []
+        if len(hist_b.index) - len(hist_a.index) != 0:
+            for _i in range(len(hist_b.index)):
+                exist = 0
+                for _j in range(len(hist_a.index)):
+                    if hist_b.index[_i] != hist_a.index[_j]:
+                        exist = 0
+                    else:
+                        exist = 1
+                        break
+                if exist == 0:
+                    to_be_drop_b.append(hist_b.index[_i])
+        hist_b = hist_b.drop(to_be_drop_b, axis=0)
+
+        return hist_a, hist_b, False
+
+    # match & drop
+    drop_list = []
+    for i in range(len(hist)):
+        if i == 0:
+            continue
+        hist[0], hist[i], drop_b = match(hist[0], hist[i])
+        # if we too drop too many hist_a elements, then we will drop hist_b
+        if drop_b:
+            drop_list.append(i)
+    drop_list.reverse()
+    print('\ndrop: '+str(drop_list))
+    for d in drop_list:
+        hist.pop(d)
 
     data = []
-    for i in range(len(hist) - (l + 1)):
+    for i in range(len(hist[0]) - (l + 1)):
         data.append([])
         # add data
         for j in range(l):
             # weekday at t+1
-            data[i].append(hist['close'].index[i + j + 1].weekday())
+            data[i].append(hist[0]['close'].index[i + j + 1].weekday())
 
-            # target asset
-            # changes at t+1 with close at t
-            data[i].append(
-                ((hist['high'].values[i + j + 1] - hist['close'].values[i + j]) / hist['close'].values[i + j]))
-            data[i].append(
-                ((hist['close'].values[i + j + 1] - hist['close'].values[i + j]) / hist['close'].values[i + j]))
-            data[i].append(
-                ((hist['open'].values[i + j + 1] - hist['close'].values[i + j]) / hist['close'].values[i + j]))
-            data[i].append(
-                ((hist['low'].values[i + j + 1] - hist['close'].values[i + j]) / hist['close'].values[i + j]))
-            data[i].append(hist['close'].values[i + j])
-            # volume at t+1
-            data[i].append(hist['volume'].values[i + j + 1])
-
-            # shanghai index
-            # shanghai index changes at t+1 with close at t
-            data[i].append(
-                ((hist_index['high'].values[i + j + 1] - hist_index['close'].values[i + j]) / hist_index['close'].values[i + j]))
-            data[i].append(
-                ((hist_index['close'].values[i + j + 1] - hist_index['close'].values[i + j]) / hist_index['close'].values[i + j]))
-            data[i].append(
-                ((hist_index['open'].values[i + j + 1] - hist_index['close'].values[i + j]) / hist_index['close'].values[i + j]))
-            data[i].append(
-                ((hist_index['low'].values[i + j + 1] - hist_index['close'].values[i + j]) / hist_index['close'].values[i + j]))
-            data[i].append(hist_index['close'].values[i + j])
-            # volume at t+1
-            data[i].append(hist_index['volume'].values[i + j + 1])
+            # past prices
+            for h in hist:
+                # changes at t+1 with close at t
+                data[i].append(
+                    ((h['high'].values[i + j + 1] - h['close'].values[i + j]) / h['close'].values[i + j]))
+                data[i].append(
+                    ((h['close'].values[i + j + 1] - h['close'].values[i + j]) / h['close'].values[i + j]))
+                data[i].append(
+                    ((h['open'].values[i + j + 1] - h['close'].values[i + j]) / h['close'].values[i + j]))
+                data[i].append(
+                    ((h['low'].values[i + j + 1] - h['close'].values[i + j]) / h['close'].values[i + j]))
+                data[i].append(h['close'].values[i + j])
+                # volume at t+1
+                data[i].append(h['volume'].values[i + j + 1])
 
         # add label
-        change = ((hist['close'].values[i + l + 1] - hist['close'].values[i + j]) / hist['close'].values[i + l])
+        change = ((hist[0]['close'].values[i + l + 1] - hist[0]['close'].values[i + j]) / hist[0]['close'].values[i + l])
         if change > 0.09:
             data[i].append(1)
         else:
             data[i].append(0)
 
-        if change > 0 and not change > 0.09:
+        if change > 0.005 and not change > 0.09:
             data[i].append(1)
         else:
             data[i].append(0)
@@ -111,7 +139,7 @@ def get_data(code=['300403', '000001'], days=200, l=1, data_file=''):
     return data
 
 
-def dl(code=['300403', '000001'], days=200, length=3, info_size=6, test_ratio=0.9, data_file=''):
+def dl(code=['300403', '000001'], days=200, length=15, label_size=4, test_ratio=0.9, data_file=''):
     # parameters:
     # length = how many history days used in prediction
     # info_size = how many factors have been included in each history day
@@ -123,20 +151,27 @@ def dl(code=['300403', '000001'], days=200, length=3, info_size=6, test_ratio=0.
         data = get_data(code, days, length)
     data = np.array(data)
     i_data = int(len(data) * test_ratio)
-    i_label = int(length * info_size)
-    input_dimension = length * info_size
+    i_label = np.shape(data)[1] - label_size
     output_dimension = np.shape(data)[1] - i_label
     train_data = data[:i_data, :i_label]
     train_label = data[:i_data, i_label:]
     test_data = data[i_data:, :i_label]
     test_label = data[i_data:, i_label:]
+
     # normalize using Z-score=(x-mu)/std
-    train_data = preprocessing.scale(train_data)
-    test_data = preprocessing.scale(test_data)
+    def zscore(m):
+        for c in range(np.shape(m)[1]):
+            mean = np.mean(m[:, c])
+            std = np.std(m[:, c])
+            for r in range(np.shape(m)[0]):
+                m[r, c] = (m[r, c]-mean)/std
+    zscore(train_data)
+    zscore(test_data)
+    print data[:10, :]
 
     # initiate the model
     model = Sequential()
-    model.add(Dense(output_dim=128, input_dim=input_dimension, init='uniform'))
+    model.add(Dense(output_dim=128, input_dim=i_label, init='uniform'))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
     model.add(Dense(output_dim=64, init='uniform'))
@@ -161,7 +196,6 @@ def dl(code=['300403', '000001'], days=200, length=3, info_size=6, test_ratio=0.
     # predict
     predict = model.predict(test_data)
     true = test_label
-    print predict
 
     fig = plt.figure(figsize=(15, 4))
     for i in range(np.shape(predict)[1]):
@@ -169,10 +203,7 @@ def dl(code=['300403', '000001'], days=200, length=3, info_size=6, test_ratio=0.
         ax.grid(True)
         x = np.linspace(1, len(predict), len(predict))
         plt.bar(x, true[:, i], alpha=0.5, color='r')
-        if i == 1:
-            plt.bar(x, (predict[:, i]-predict[:, i+1]), alpha=0.5, color='g')
-        else:
-            plt.bar(x, predict[:, i], alpha=0.5, color='g')
+        plt.bar(x, predict[:, i], alpha=0.5, color='g')
     plt.show()
 
     return
